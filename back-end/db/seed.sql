5fa2c6f740ab7d2a33d78c690924033e3497efa3
Fig <bethanytrr@gmail.com>
2023-11-15 01:16:27 -0500

    Updated Seed.sql to reflect more data.

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,9 +1,13 @@
 \c swords_dev
 
 INSERT INTO swords (name, maker, price, is_upgraded, is_cursed, rarity) VALUES
-  ('Excalibur', 'Merlin Smith', 1000, true, false, 5),
-  ('Doombringer', 'Blacksmith McGee', 500, false, false, 3),
-  ('Shadowstrike', 'Silent Forger', 800, true, false, 4),
-  ('Frostbite', 'Ice Queen Crafts', 1200, true, false, 5),
-  ('Serpent''s Bite', 'Venom Smithy', 700, false, true, 3),
-  ('Moonlight''s Grace', 'Celestial Smiths', 1500, true, false, 5);
+  ('Excalibur', 'Merlin Smith', 1000, true, false, 4),
+  ('Doombringer', 'Blacksmith McGee', 500, false, true, 1),
+  ('Shadowstrike', 'Silent Forger', 800, true, false, 3),
+  ('Frostbite', 'Ice Queen Crafts', 1200, true, false, 1),
+  ('Serpent''s Bite', 'Venom Smithy', 700, false, true, 2),
+  ('Thunderstrike', 'Stormforge Artisans', 900, false, false, 2),
+  ('Blazeblade', 'Inferno Smiths', 110, true, false, 1),
+  ('Abyssal Edge', 'Dark Artistry', 2300, true, true, 5),
+  ('Solar Flare', 'Sunfire Forgers', 1600, true, false, 5),
+  ('Moonlight''s Grace', 'Celestial Smiths', 1200, true, false, 4);
--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,9 @@
-// server.js
-// DEPENDENCIES
+
 const app = require("./app.js");
 
-// CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-// LISTEN
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
